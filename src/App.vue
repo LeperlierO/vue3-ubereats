d5aa924bb032bf6ad14895b6acf231e8d717e17f
LeperlierO <leperlierolivier3@gmail.com>
2024-01-30 11:44:48 +0100

    Part 1 - 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <restaurant-row/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import RestaurantRow from './components/RestaurantRow.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RestaurantRow
   }
 }
 </script>
 
 <style>
+@import url('./style/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto', sans-serif;
+  padding: 20px;
 }
 </style>
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="restaurant--card">
+    <div class="restaurant--image">
+
+    </div>
+    <div class="restaurant--informations">
+      <div class="top">
+        <p class="name">
+          Subway
+        </p>
+        <p class="note">
+          <span>
+            4.5
+          </span>
+        </p>
+      </div>
+      <p class="tme">
+        20-30min
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RestaurantCard",
+}
+</script>
+
+<style lang="scss">
+    .restaurant--card { 
+        width: 32%;
+        height: 30vh;
+
+        p {
+          margin: 0px;
+        }
+
+        .restaurant--image{
+          height: 70%;
+          border-radius: 15px;
+          background-size: cover;
+          background-position: center;
+          background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==");
+        }
+
+        .restaurant--informations{
+          
+          .top {
+            margin: 10px 0;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .note {
+              background-color: #eeeeee;
+              height: 30px;
+              width: 30px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              font-size: 0.8rem;
+              border-radius: 50%;
+            }
+          }
+
+          .time {
+            font-size: 0.9rem;
+          }
+        }
+    }
+
+</style>
